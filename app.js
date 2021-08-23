75b8872a7df9ba4be0532687538021c80e1983a3
Vince Rivera <riverav@me.com>
2021-08-23 13:04:28 -0500

    added fs module and created new src for page template

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
 const [name, github] = profileDataArgs;
+
 // console.log(profileDataArgs);
 
 // const printProfileData = profileDataArr => {
@@ -10,24 +15,10 @@ const [name, github] = profileDataArgs;
   
 //   printProfileData(profileDataArgs);
 
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html>
-    <html lang-"en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-    </head>
 
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
     
