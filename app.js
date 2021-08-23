dc14015b567d02d87382a043013583b8a59ef762
Vince Rivera <riverav@me.com>
2021-08-23 13:47:56 -0500

    Added inquirer module and created first question

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
 
 // console.log(profileDataArgs);
 
@@ -16,9 +14,18 @@ const [name, github] = profileDataArgs;
 //   printProfileData(profileDataArgs);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-    
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
